d9b9be117687285d617630626a892d975fd04729
hooman124 <hooman.kh10@gmail.com>
2026-09-21 12:58:26 +0330

    Update number_input.dart

--- lib/widgets/number_input.dart
+++ lib/widgets/number_input.dart
@@ -5,16 +5,42 @@ class NumberInput extends StatelessWidget {
   final TextEditingController controller;
   final String label;
   final String suffix;
-  final bool decimal;
-  const NumberInput({super.key, required this.controller, required this.label, this.suffix = '', this.decimal = true});
+  final String? hint;
+
+  const NumberInput({
+    super.key,
+    required this.controller,
+    required this.label,
+    required this.suffix,
+    this.hint,
+  });
 
   @override
   Widget build(BuildContext context) {
     return TextField(
       controller: controller,
-      keyboardType: TextInputType.numberWithOptions(decimal: decimal),
-      inputFormatters: [FilteringTextInputFormatter.allow(RegExp(r'[0-9.]'))],
-      decoration: InputDecoration(labelText: label, suffixText: suffix.isEmpty ? null : suffix),
+      keyboardType: const TextInputType.numberWithOptions(
+        decimal: true,
+      ),
+      textDirection: TextDirection.ltr,
+      textAlign: TextAlign.right,
+      inputFormatters: [
+        FilteringTextInputFormatter.allow(
+          RegExp(r'[0-9.]'),
+        ),
+      ],
+      decoration: InputDecoration(
+        labelText: label,
+        hintText: hint,
+        suffixText: suffix,
+        labelStyle: const TextStyle(
+          fontWeight: FontWeight.w600,
+        ),
+        suffixStyle: const TextStyle(
+          fontWeight: FontWeight.w600,
+          color: Color(0xFF9A7525),
+        ),
+      ),
     );
   }
 }
