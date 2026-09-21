0b6f9f96e3eb9984e4c09d8954eca094d8544d87
hooman124 <hooman.kh10@gmail.com>
2026-09-21 12:59:43 +0330

    Update gold_formulas.dart

--- lib/utils/gold_formulas.dart
+++ lib/utils/gold_formulas.dart
@@ -1,8 +1,50 @@
 class GoldFormulas {
-  static double rawGoldValue({required double weight, required double pricePerGram}) => weight * pricePerGram;
-  static double makingCharge({required double rawValue, required double makingPercent}) => rawValue * makingPercent / 100;
-  static double sellerProfit({required double rawValue, required double makingValue, required double profitPercent}) => (rawValue + makingValue) * profitPercent / 100;
-  static double tax({required double taxBase, required double taxPercent}) => taxBase * taxPercent / 100;
-  static double finalPrice({required double rawValue, required double makingValue, required double profitValue, required double taxValue}) => rawValue + makingValue + profitValue + taxValue;
-  static double convertKarat({required double weight, required double fromKarat, required double toKarat}) => toKarat <= 0 ? 0 : weight * fromKarat / toKarat;
+  static double rawGoldValue({
+    required double weight,
+    required double pricePerGram,
+  }) {
+    return weight * pricePerGram;
+  }
+
+  static double makingCharge({
+    required double rawValue,
+    required double makingPercent,
+  }) {
+    return rawValue * makingPercent / 100;
+  }
+
+  static double sellerProfit({
+    required double rawValue,
+    required double makingValue,
+    required double profitPercent,
+  }) {
+    return (rawValue + makingValue) * profitPercent / 100;
+  }
+
+  // مالیات = 10٪ × (اجرت ساخت + سود فروشنده)
+  static double tax({
+    required double makingValue,
+    required double profitValue,
+  }) {
+    return (makingValue + profitValue) * 0.10;
+  }
+
+  static double finalPrice({
+    required double rawValue,
+    required double makingValue,
+    required double profitValue,
+    required double taxValue,
+  }) {
+    return rawValue + makingValue + profitValue + taxValue;
+  }
+
+  static double convertKarat({
+    required double weight,
+    required double fromKarat,
+    required double toKarat,
+  }) {
+    if (toKarat <= 0) return 0;
+
+    return weight * fromKarat / toKarat;
+  }
 }
